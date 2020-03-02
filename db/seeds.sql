1aef64829d661f240b4fb631086a3f6a21ab0c20
Designer <visualdesign3r@gmail.com>
2020-03-01 23:51:07 -0800

    uploading files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burger (name) VALUES ('Bacon Burger');
+INSERT INTO burger (name) VALUES ('Guac Burger');
+INSERT INTO burger (name) VALUES ('Cheeseburger');
+
+
+-- INSERT INTO burger (name, sleepy) VALUES ('Sylvester', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
+-- INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
+-- INSERT INTO cats (name) VALUES ('Owens');
